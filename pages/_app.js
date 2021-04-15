f5a81f27447b506403f809d2eb23154c40b4722c
Nathan Davies <nathan.davies@manifesto.co.uk>
2021-04-15 13:32:01 +0100

    Nextjs: basic SEO information

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Header from "../components/header"
 import PhaseBanner from "../components/phase-banner"
 import SkipLink from "../components/skip-link"
+import Head from "next/head"
 import "../styles/global.scss"
 
 function HousingRegisterApp({ Component, pageProps }) {
@@ -8,15 +9,21 @@ function HousingRegisterApp({ Component, pageProps }) {
 
   return (
     <>
-      <SkipLink />
-      <Header />
-      {hasPhaseBanner && <PhaseBanner />}
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_NAME} | Hackney Council</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
+      <>
+        <SkipLink />
+        <Header />
+        {hasPhaseBanner && <PhaseBanner />}
 
-      <main id="main-content" className="lbh-main-wrapper">
-        <div className="lbh-container">
-          <Component {...pageProps} />
-        </div>
-      </main>
+        <main id="main-content" className="lbh-main-wrapper">
+          <div className="lbh-container">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </>
     </>
   )
 }

403d8cc88ad117342bbd36b6376700ee5bb1070c
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-04-27 11:39:44 +0100

    Fix for login button

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/layout/resident-layout';
-import Button from "../components/button"
+import { ButtonLink } from "../components/button"
 import { HeadingOne } from "../components/headings"
 import Paragraph from "../components/paragraph"
 
@@ -8,7 +8,7 @@ export default function Login(): JSX.Element {
     <Layout>
       <HeadingOne content="Staff login" />
       <Paragraph content="<strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit. Pellentesque mi ex, maximus tempus condimentum eget, volutpat eu nunc. Mauris tincidunt, neque quis viverra ultricies, tellus lacus fringilla tortor, at blandit purus diam et augue." />
-      <Button href="/applications">Login</Button>
+      <ButtonLink href="/applications">Login</ButtonLink>
     </Layout>
   )
 }

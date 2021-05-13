8a57fdd7a2daa8b0d9cde5577d0d64ffb4ca680f
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-05-13 10:55:41 +0100

    Basic setup for cognito with prebuilt ui

--- components/layout/resident-layout.tsx
+++ components/layout/resident-layout.tsx
@@ -4,26 +4,30 @@ import SkipLink from "../skip-link"
 import Header from "../header"
 import { hasPhaseBanner } from "../../lib/utils/phase-banner"
 import React, { ReactNode } from "react"
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 interface ResidentLayoutProps {
-  breadcrumbs?: { href: string, name: string}[]
+  breadcrumbs?: { href: string, name: string }[]
   children: ReactNode
 }
 
-export default function ResidentLayout({ breadcrumbs, children }: ResidentLayoutProps): JSX.Element {
+const ResidentLayout = ({ breadcrumbs, children }: ResidentLayoutProps): JSX.Element => {
   return (
     <>
       <SkipLink />
       <Header />
       {hasPhaseBanner() && <PhaseBanner />}
-      
+
       {breadcrumbs && breadcrumbs.length > 0 && <Breadcrumbs items={breadcrumbs} />}
 
       <main id="main-content" className="lbh-main-wrapper">
         <div className="lbh-container">
+          <AmplifySignOut />
           {children}
         </div>
       </main>
     </>
   )
 }
+
+export default withAuthenticator(ResidentLayout)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,18 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import { FC } from "react"
 import "../styles/global.scss"
+import Amplify, { Auth } from 'aws-amplify';
 
 //export default function HousingRegisterApp({ Component, pageProps }: AppProps): JSX.Element {
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => {
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+  Amplify.configure({
+    Auth: {
+      region: 'eu-west-2',
+      userPoolId: 'eu-west-2_Zvje6WnHM',
+      userPoolWebClientId: '523t1n3k7tmu22la4eunnarhij',
+    }
+  });
+
   return (
     <>
       <Head>

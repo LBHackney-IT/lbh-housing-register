d337435e02c59fe884b99424f831d37e146d9444
Dan James <danj@deeson.co.uk>
2021-06-11 15:17:11 +0100

    Load the current user and application into the store at app init time (incomplete)

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -2,20 +2,26 @@ import additionalResidents from './additionalResidents';
 import resident from './resident';
 import { MainResident, Resident } from '../types/resident';
 import { combineReducers } from 'redux';
-import { createWrapper, Context } from 'next-redux-wrapper';
+import { createWrapper } from 'next-redux-wrapper';
 import { configureStore } from '@reduxjs/toolkit';
+import cognitoUser, { CognitoUserInfo } from './cognitoUser';
 
 export interface Store {
   additionalResidents: Resident[];
   resident: MainResident;
+  cognitoUser: CognitoUserInfo | null;
 }
 
-const reducer = combineReducers({
+const reducer = combineReducers<Store>({
   additionalResidents: additionalResidents.reducer,
   resident: resident.reducer,
+  cognitoUser: cognitoUser.reducer,
 });
 
-const store = () => configureStore({ reducer });
+const makeStore = () => configureStore({ reducer });
 
-export const wrapper = createWrapper(store);
-export default store;
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const wrapper = createWrapper(makeStore);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { wrapper } from '../lib/store';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { FC } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import '../styles/global.scss';
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+import { useAppDispatch, useAppSelector } from '../lib/store/hooks';
+import { loadUser } from '../lib/store/cognitoUser';
+import { loadApplicaiton } from '../lib/store/application';
 
-//export default function HousingRegisterApp({ Component, pageProps }: AppProps): JSX.Element {
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+function App({ Component, pageProps }: AppProps): ReactElement {
   Amplify.configure({
     Auth: {
       region: process.env.NEXT_PUBLIC_AWS_REGION,
@@ -14,6 +16,24 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
       userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
     },
   });
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
+
+  const user = useAppSelector((state) => state.cognitoUser);
+  useEffect(() => {
+    if (user) {
+      //     // don't do this without an effect. You just got rate limited.
+      //     Auth.currentAuthenticatedUser().then((user: CognitoUser) => {
+      //       Auth.updateUserAttributes(user, {
+      //         'custom:application_id': '0b8bf684-e3ae-499a-af05-fafcefca6f46',
+      //       });
+      //     });
+      dispatch(loadApplicaiton(user.attributes['custom:application_id']));
+    }
+  }, [user]);
 
   return (
     <>
@@ -24,11 +44,14 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
       </Head>
-      <>
-        <Component {...pageProps} />
-      </>
+
+      <Component {...pageProps} />
     </>
   );
-};
+}
 
-export default wrapper.withRedux(WrappedApp);
+export default wrapper.withRedux(App);
+
+/*
+ * So when the page loads we'll want to retrieve anything that we know.
+ */

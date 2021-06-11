022e9b9f8c0ce4f26e9190e3ce073f68dbbd75b4
Dan James <danj@deeson.co.uk>
2021-06-11 15:12:23 +0100

    Added a next JS API endpoint for getting applications from the housing register API

--- pages/api/applications/[id].tsx
+++ pages/api/applications/[id].tsx
@@ -1,16 +1,28 @@
 import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
-import { updateApplication } from '../../../lib/gateways/applications-api';
+import {
+  getApplication,
+  updateApplication,
+} from '../../../lib/gateways/applications-api';
+import AccessDeniedPage from '../../access-denied';
 
 const endpoint: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   switch (req.method) {
+    case 'GET': {
+      // TODO IMPORTANT we need an access check here. User should pass us a token that we can validate against their application.
+      const id = req.query.id as string;
+      const data = await getApplication(id);
+      res.status(StatusCodes.OK).json(data);
+
+      break;
+    }
     case 'PATCH':
       try {
         const application = JSON.parse(req.body);
-        const id = req.query.id;
+        const id = req.query.id as string;
         const data = await updateApplication(application, id);
         res.status(StatusCodes.OK).json(data);
       } catch (error) {

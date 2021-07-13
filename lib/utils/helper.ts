fded710a94bd8b5a071b46ff7043a875fd0582dc
Dan James <danj@deeson.co.uk>
2021-07-13 08:53:52 +0100

    Remove functions relating to the creation/updating of applications that should now be handled with thunks

--- lib/utils/helper.ts
+++ lib/utils/helper.ts
@@ -1,37 +1,3 @@
-import {
-    CreateApplicationRequest
-} from '../../domain/application';
-import { Resident } from '../types/resident';
-
-// convert an application in react state into an application for the api
-export const constructApplication = (applicants: Resident[]) => {
-  const personal = applicants[0].formData['personal-details'];
-  const address = applicants[0].formData['address-details'];
-  const contactInformation = {
-    emailAddress: applicants[0].formData[''],
-    phoneNumber: '',
-    preferredMethodOfContact: '',
-  };
-
-  const applicant = {
-    personal,
-    address,
-    contactInformation,
-  };
-  const application: CreateApplicationRequest = {
-    status: 'Pending',
-    applicant,
-    otherMembers:
-      applicants.length > 1
-        ? applicants
-            .slice(1)
-            .map((resident) => resident?.formData['personal-details'])
-        : [],
-  };
-
-  return application;
-};
-
 export const extractAdditionalResidentFromData = (
   data: any,
   countOfApplicants: any

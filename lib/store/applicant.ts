c7cc77ac4ce49ae9453da4a2a39dfc8bf446be72
Dan James <danj@deeson.co.uk>
2021-07-13 08:44:05 +0100

    Clean up circular reference in reducer.

--- lib/store/applicant.ts
+++ lib/store/applicant.ts
@@ -6,6 +6,22 @@ import { AGREEMENT } from '../utils/form-data';
 
 const initialState: Applicant = {};
 
+function applyQuestions(state:  Applicant | undefined, activeStepId: string, values: FormikValues): Applicant {
+  return {
+    ...(state || {}),
+    questions: [
+      ...(state?.questions?.filter(
+        (question) =>
+          !question.id?.startsWith(`${activeStepId}/`)
+      ) || []),
+      ...Object.entries(values).map(([id, answer]) => ({
+        id: `${activeStepId}/${id}`,
+        answer: JSON.stringify(answer),
+      })),
+    ],
+  }
+}
+
 const slice = createSlice({
   name: 'applicant',
   initialState: initialState as Applicant | undefined,
@@ -13,30 +29,12 @@ const slice = createSlice({
     /**
      * Agree to terms and conditions
      */
-    agree: (state, action) =>
-      slice.caseReducers.updateWithFormValues(state, {
-        ...action,
-        payload: { activeStepId: AGREEMENT, values: { agree: true } },
-      }),
+    agree: (state, action) => applyQuestions(state, AGREEMENT, {agree: true}),
 
-    // TODO how do we tell which applicant we'll be updating? We should maybe rename "applicant" to "mainApplicant"?
-    // TODO This function is going to get long, procedural, and will directly relate to mapApplicantToValues.
     updateWithFormValues: (
       state,
       action: PayloadAction<{ activeStepId: string; values: FormikValues }>
-    ) => ({
-      ...(state || {}),
-      questions: [
-        ...(state?.questions?.filter(
-          (question) =>
-            !question.id?.startsWith(`${action.payload.activeStepId}/`)
-        ) || []),
-        ...Object.entries(action.payload.values).map(([id, answer]) => ({
-          id: `${action.payload.activeStepId}/${id}`,
-          answer: JSON.stringify(answer),
-        })),
-      ],
-    }),
+    ) =>  applyQuestions(state, action.payload.activeStepId, action.payload.values)
   },
 });
 

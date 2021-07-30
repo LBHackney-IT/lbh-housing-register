b0af18142897476e9158ebe74c8e6a47abf10e7b
rashthedude <rashid.aden.omar@gmail.com>
2021-07-30 02:12:49 +0100

    further progress

--- pages/apply/[resident]/summary.tsx
+++ pages/apply/[resident]/summary.tsx
@@ -17,19 +17,248 @@ interface DisplayProps {
   };
 }
 
-export function DisplayInfo({ question }: DisplayProps): JSX.Element {
+// Date types have been ignored for the most parts and need serious attention!!!
+
+const retrieveSectionName = (question: any) => {
+  return question['id'].substr(question['id'].lastIndexOf('/') + 1);
+};
+
+const normalizeString = (answer: string) => {
+  return answer.replace(/[^a-zA-Z ]/g, '');
+};
+
+export function ImmigrationStatus(data) {
+  const formulator = (question: any) => {
+    if (question) {
+      if (retrieveSectionName(question) === 'citizenship') {
+        if (normalizeString(question['answer']) === 'european') {
+          return 'an EEA';
+        }
+
+        if (normalizeString(question['answer']) === 'british') {
+          return 'a British';
+        }
+      }
+
+      if (retrieveSectionName(question) === 'uk-studying') {
+        if (normalizeString(question['answer']) === 'yes') {
+          return 'currently studying in the UK';
+        }
+
+        if (normalizeString(question['answer']) === 'no') {
+          return '';
+        }
+      }
+
+      if (retrieveSectionName(question) === 'settled-status') {
+        if (normalizeString(question['answer']) === 'yes') {
+          return 'and I have a pre-settled status';
+        }
+
+        if (normalizeString(question['answer']) === 'no') {
+          return '';
+        }
+      }
+    }
+  };
+
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">Immigration status</h4>
+      <p className="lbh-body-m">{`I am ${formulator(data[0])} citizen ${
+        data[1] ? formulator(data[1]) : ''
+      } ${data[2] ? formulator(data[2]) : ''}`}</p>
+    </div>
+  );
+}
+
+export function ResidentialStatus(data) {
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">Residential Status</h4>
+      <p className="lbh-body-m"></p>
+    </div>
+  );
+}
+
+export function CurrentAccommodation(data) {
+  console.log('Current Accommodation data', data);
+
+  const formulator = (question: any) => {
+    if (retrieveSectionName(question) === 'living-situation') {
+      switch (normalizeString(question['answer'])) {
+        case 'livingwithparents':
+          return 'with parents';
+        case 'livingwithfriends':
+          return 'with friends';
+        case 'livingwithrelatives':
+          return 'with relatives';
+        case 'temporaryaccommodation':
+          return 'temporary accommodation';
+        case 'privaterental':
+          return 'private rental';
+        case 'owneroccupier':
+          return 'owner occupier';
+        case 'nofixedabode':
+          return 'no fixed abode';
+        case 'squatter':
+          return 'squatter';
+        case 'unauthorisedoccupant':
+          return 'unauthorised occupant';
+        case 'tiedaccommodation':
+          return 'tied accommodation';
+      }
+    }
+
+    if (retrieveSectionName(question) === 'home') {
+      switch (normalizeString(question['answer'])) {
+        case 'house':
+          return 'house';
+      }
+    }
+
+    if (retrieveSectionName(question) === 'home-floor') {
+      return question['answer'];
+    }
+
+    if (retrieveSectionName(question) === 'home-how-many-people-share') {
+      return question['answer'];
+    }
+  };
+
+  return (
+    <>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <h4 className="lbh-heading-h4">Current Accommodation</h4>
+        <p className="lbh-body-m">{`I am living with ${formulator(
+          data[0]
+        )}`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`I live in a ${formulator(data[1])}`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`My home is on floor ${formulator(
+          data[2]
+        )}`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`I share my home with ${formulator(
+          data[3]
+        )} people not included in this application`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`My home has ${data[4]['answer']} bedroom(s)`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`My home has ${data[5]['answer']} bathrooms`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`My home has ${data[6]['answer']} kitchen`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`My home has ${data[7]['answer']} other rooms`}</p>
+      </div>
+      <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+        <p className="lbh-body-m">{`I have ${data[8]['answer']} rooms for my own personal use`}</p>
+      </div>
+    </>
+  );
+}
+
+export function MySituation(data) {
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">My Situation</h4>
+      <p className="lbh-body-m"></p>
+    </div>
+  );
+}
+
+export function IncomeSavings(data) {
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">Income & savings</h4>
+      <p className="lbh-body-m"></p>
+    </div>
+  );
+}
+
+export function Employment(data) {
+  const formulator = (question: any) => {
+    if (retrieveSectionName(question) === 'employment') {
+      switch (normalizeString(question['answer'])) {
+        case 'employed':
+          return 'full time employed';
+        case 'self-employed':
+          return 'self employed';
+        case 'fulltimestudent':
+          return 'a full time student';
+        case 'unemployed':
+          return 'unemployed';
+        case 'retired':
+          return 'retired';
+      }
+    }
+  };
+
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">Employment</h4>
+      <p className="lbh-body-m">{`I am ${formulator(data[0])}`}</p>
+    </div>
+  );
+}
+
+export function Health(data) {
+  const formulator = (question: any) => {
+    if (retrieveSectionName(question) === 'medical-needs') {
+      if (normalizeString(question['answer']) === 'yes') {
+        return 'I do have a medical needs that affects my housing needs';
+      }
+      if (normalizeString(question['answer']) === 'no') {
+        return 'I do not have any medical needs';
+      }
+    }
+  };
+
+  return (
+    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
+      <h4 className="lbh-heading-h4">Health</h4>
+      <p className="lbh-body-m">{formulator(data[0])}</p>
+    </div>
+  );
+}
+
+export function DisplayInfo({ question }: any): JSX.Element {
   const formatHeader = (header: any) => {
     header = header.replace(/-|\s+/g, ' ').toUpperCase();
     header = header.substring(0, header.lastIndexOf('/'));
     return header;
   };
 
-  return (
-    <div style={{ borderBottom: '1px solid', color: '#b1b4b6' }}>
-      <h4 className="lbh-heading-h4">{formatHeader(question?.id)}</h4>
-      <p className="lbh-body-m">{question?.answer}</p>
-    </div>
-  );
+  const answerBuilder = (question: any) => {
+    switch (
+      question[0]['id'].substring(0, question[0]['id'].lastIndexOf('/'))
+    ) {
+      case 'immigration-status':
+        return ImmigrationStatus(question);
+      case '':
+        return ResidentialStatus(question);
+      case 'current-accommodation':
+        return CurrentAccommodation(question);
+      case '':
+        return MySituation(question);
+      case 'income-savings':
+        return IncomeSavings(question);
+      case 'employment':
+        return Employment(question);
+      case 'medical-needs':
+        return Health(question);
+    }
+  };
+
+  return <>{answerBuilder(question)}</>;
 }
 
 const UserSummary = (): JSX.Element => {
@@ -59,9 +288,24 @@ const UserSummary = (): JSX.Element => {
     },
   ];
 
-  // Create a custom object that makes it easy to iterate over for DisplayInfo
-  let questions = currentResident.questions;
+  let questions: any = currentResident.questions;
   questions = questions?.slice(1);
+
+  const groupedSectionAnswers = questions?.reduce(
+    (result: any, currentValue: any) => {
+      let groupSection = currentValue.id.substring(
+        0,
+        currentValue.id.lastIndexOf('/')
+      );
+
+      result[groupSection] = result[groupSection] || [];
+      result[groupSection].push(currentValue);
+      return result;
+    },
+    {}
+  );
+
+  // console.log('what is groupedAnswers', groupedSectionAnswers);
 
   return (
     <Layout breadcrumbs={breadcrumbs}>
@@ -110,9 +354,12 @@ const UserSummary = (): JSX.Element => {
         </div>
       </dl>
       <br />
-      {questions?.map((question, index) => {
-        return <DisplayInfo question={question} key={index} />;
+      {Object.keys(groupedSectionAnswers).map((question, index) => {
+        return (
+          <DisplayInfo question={groupedSectionAnswers[question]} key={index} />
+        );
       })}
+
       <ButtonLink href="submit">I confirm this is correct</ButtonLink>
       <DeleteLink content="Delete this information" onDelete={onDelete} />
     </Layout>

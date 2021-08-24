62cf5e65eab29de1650b8aab686de7ac602e9cda
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-08-24 15:46:26 +0100

    Add additional questions before submit

---
+++ pages/apply/submit/additional-questions.tsx
@@ -0,0 +1,51 @@
+import { FormikValues } from 'formik';
+import { useRouter } from 'next/router';
+import { HeadingOne } from '../../../components/content/headings';
+import Form from '../../../components/form/form';
+import Layout from '../../../components/layout/resident-layout';
+import { FormID, getFormData } from '../../../lib/utils/form-data';
+import Custom404 from '../../404';
+import { useAppDispatch, useAppSelector } from '../../../lib/store/hooks';
+import {
+  getQuestionsForFormAsValues,
+  updateWithFormValues,
+} from '../../../lib/store/applicant';
+
+export default function AdditonalQuestions() {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const applicant = useAppSelector((store) => store.application.mainApplicant);
+
+  if (!applicant) {
+    return <Custom404 />;
+  }
+
+  const initialValues = {
+    ...getQuestionsForFormAsValues(FormID.ADDITIONAL_QUESTIONS, applicant),
+  };
+
+  const returnHref = '/apply/submit/ethnicity-questions';
+  const onSubmit = (values: FormikValues) => {
+    dispatch(
+      updateWithFormValues({
+        formID: FormID.ADDITIONAL_QUESTIONS,
+        personID: applicant.person?.id || '',
+        values,
+        markAsComplete: true,
+      })
+    );
+    router.push(returnHref);
+  };
+
+  return (
+    <Layout pageName="Before you submit">
+      <HeadingOne content="Before you submit" />
+      <Form
+        initialValues={initialValues}
+        buttonText="Save and continue"
+        formData={getFormData(FormID.ADDITIONAL_QUESTIONS)}
+        onSubmit={onSubmit}
+      />
+    </Layout>
+  );
+}

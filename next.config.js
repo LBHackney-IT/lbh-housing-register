669283e3dc9bdc1798d4f82e45cb5fc10a372864
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-08-26 18:35:32 +0100

    Update dependencies, next config for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 module.exports = {
+  distDir: 'build/_next',
+  target: 'server',
+  poweredByHeader: false,
+  reactStrictMode: true,
   future: {
     webpack5: true,
   },
-  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+        ],
+      },
+    ];
+  },
 };

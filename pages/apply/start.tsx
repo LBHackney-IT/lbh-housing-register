cda563776639d9931497c8007edeee95aa21d2f8
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-09-02 15:46:17 +0100

    Update package dependencies

--- pages/apply/start.tsx
+++ pages/apply/start.tsx
@@ -1,4 +1,4 @@
-import { Auth } from 'aws-amplify';
+import Auth from '@aws-amplify/auth';
 import { FormikValues } from 'formik';
 import { useRouter } from 'next/router';
 import { HeadingOne } from '../../components/content/headings';

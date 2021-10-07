83fbe969c175b9ffba9a6b1e96c1c262a0d05be5
Thomas Morris <thomas.morris@manifesto.co.uk>
2021-10-07 11:34:20 +0100

    NEXT_PUBLIC not available, add static value

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,14 @@ export default class AppDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","${process.env.NEXT_PUBLIC_GTM_ID}");`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-P3HRQWH");`,
             }}
           />
         </Head>
         <body className="govuk-template__body lbh-template__body js-enabled">
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P3HRQWH"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}

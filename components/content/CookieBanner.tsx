047b62c166d009877a76879e3719891daf025798
gdbarnes <gregdbarnes@gmail.com>
2021-10-14 17:03:40 +0100

    Add cookie banner using lbh-frontend component

---
+++ components/content/CookieBanner.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react';
+import Paragraph from '../content/paragraph';
+
+export default function CookieBanner(): JSX.Element {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (window !== undefined) {
+      const CookieBanner = require('lbh-frontend').CookieBanner;
+      new CookieBanner(ref.current).init();
+    }
+  }, []);
+
+  return (
+    <section
+      className="lbh-cookie-banner"
+      data-module="lbh-cookie-banner"
+      ref={ref}
+    >
+      <div className="lbh-container">
+        <div className="govuk-grid-row">
+          <div className="lbh-cookie-banner__content govuk-grid-column-two-thirds-from-desktop">
+            <Paragraph>
+              We use cookies to ensure you have the best experience. For full
+              details see our{' '}
+              <a href="https://hackney.gov.uk/privacy">privacy statement</a>.
+            </Paragraph>
+          </div>
+          <div className="lbh-cookie-banner__button-wrapper govuk-grid-column-one-third-from-desktop">
+            <button
+              type="button"
+              className="govuk-button lbh-cookie-banner__button lbh-button govuk-button--secondary lbh-button--secondary"
+              data-module="govuk-button"
+              data-behavior="lbh-cookie-close"
+            >
+              Accept and close
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- components/layout/resident-layout.tsx
+++ components/layout/resident-layout.tsx
@@ -8,6 +8,7 @@ import PhaseBanner from '../phase-banner';
 import SkipLink from '../skip-link';
 import Seo from '../seo';
 import Footer from '../footer';
+import CookieBanner from '../content/CookieBanner';
 import { exit } from '../../lib/store/auth';
 
 interface ResidentLayoutProps {
@@ -48,6 +49,7 @@ export default function ResidentLayout({
       </main>
 
       <Footer referenceNumber={application.reference ?? ''} />
+      <CookieBanner />
     </>
   );
 }

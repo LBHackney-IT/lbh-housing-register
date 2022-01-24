c20070e8d635aff16377363933aae627c8b9b25b
Dan James <danj@deeson.co.uk>
2022-01-24 15:18:32 +0000

    Updated TS type to match API.

--- domain/HousingApi.ts
+++ domain/HousingApi.ts
@@ -204,6 +204,13 @@ export interface Application {
    * @memberof Application
    */
   calculatedBedroomNeed?: number;
+
+  /**
+   *
+   * @type {boolean}
+   * @memberof Application
+   */
+  importedFromLegacyDatabase?: boolean;
 }
 
 /**

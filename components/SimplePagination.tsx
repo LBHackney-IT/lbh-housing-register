824b9b8a6850e2078063df05194b027e1724a0a7
gdbarnes <gregdbarnes@gmail.com>
2022-02-04 20:02:18 +0000

    Simple pagination component clean up

--- components/SimplePagination.tsx
+++ components/SimplePagination.tsx
@@ -1,26 +1,3 @@
-// interface PaginationItemProps {
-//   page: number;
-//   pageUrl: URL;
-//   isCurrent: boolean;
-// }
-
-// const PaginationItem = ({ page, pageUrl, isCurrent }: PaginationItemProps) => {
-//   return (
-//     <li className="lbh-pagination__item">
-//       <a
-//         className={`lbh-pagination__link lbh-pagination__link${
-//           isCurrent ? '--current' : ''
-//         }`}
-//         href={`${pageUrl.href}`}
-//         aria-current={isCurrent}
-//         aria-label={`Page ${page}, ${isCurrent ? 'current page' : ''}`}
-//       >
-//         {page}
-//       </a>
-//     </li>
-//   );
-// };
-
 interface SimplePaginationProps {
   totalItems: number;
   page: number;
@@ -29,7 +6,6 @@ interface SimplePaginationProps {
   pageStartOffSet: number;
   pageEndOffSet: number;
   pageUrl: string;
-  parameters: URLSearchParams;
 }
 
 const SimplePagination = ({
@@ -39,34 +15,12 @@ const SimplePagination = ({
   pageStartOffSet,
   pageEndOffSet,
   pageUrl,
-  parameters,
 }: SimplePaginationProps) => {
   const previousPageUrl = new URL(pageUrl);
   previousPageUrl.searchParams.append('page', (page - 1).toString());
 
   const nextPageUrl = new URL(pageUrl);
   nextPageUrl.searchParams.append('page', (page + 1).toString());
-
-  // var paginationItems = [];
-  // for (var i = 1; i <= totalNumberOfPages; i++) {
-  //   const urlWithParams = new URL(pageUrl);
-  //   urlWithParams.searchParams.append('page', i.toString());
-
-  //   if (Array.from(parameters).length > 0) {
-  //     for (var pair of parameters.entries()) {
-  //       urlWithParams.searchParams.append(pair[0], pair[1]);
-  //     }
-  //   }
-
-  //   paginationItems.push(
-  //     <PaginationItem
-  //       key={i}
-  //       page={i}
-  //       isCurrent={i == page}
-  //       pageUrl={urlWithParams}
-  //     />
-  //   );
-  // }
 
   return (
     <>

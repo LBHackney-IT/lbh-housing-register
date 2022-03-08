97223172c201ec00b030daca9fc4e0539effa504
gdbarnes <gregdbarnes@gmail.com>
2022-03-08 15:18:10 +0000

    Do not update application with error on disqualification and blacklist disqualify application for auto update

--- lib/store/application.ts
+++ lib/store/application.ts
@@ -46,11 +46,11 @@ export const disqualifyApplication = createAsyncThunk(
       id: id,
       status: ApplicationStatus.DISQUALIFIED,
     };
-    const res = await fetch(`/api/applications/${id}`, {
+    await fetch(`/api/applications/${id}`, {
       method: 'PATCH',
       body: JSON.stringify(request),
     });
-    return (await res.json()) as Application;
+    // return (await res.json()) as Application;
   }
 );
 
@@ -214,7 +214,8 @@ export const autoSaveMiddleware: Middleware<
     function blacklist(type: string) {
       return (
         type.startsWith(loadApplication.typePrefix) ||
-        type.startsWith(updateApplication.typePrefix)
+        type.startsWith(updateApplication.typePrefix) ||
+        type.startsWith(disqualifyApplication.typePrefix)
       );
     }
 

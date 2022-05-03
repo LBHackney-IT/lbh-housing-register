500cc9cc1a3aee330abdfb2c42276646c5279994
Dan James <danj@deeson.co.uk>
2022-05-03 14:34:04 +0100

    record console events too please

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -18,4 +19,5 @@ Sentry.init({
   // that it will also get attached to your source maps
 
   environment: process.env.NEXT_PUBLIC_ENV,
+  integrations: [new CaptureConsoleIntegration()],
 });

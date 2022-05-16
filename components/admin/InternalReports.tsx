b7749b04a8078511d5dc9735ae6dd65c0d1800e7
gdbarnes <gregdbarnes@gmail.com>
2022-05-16 15:00:03 +0100

    Stop trying to download the file with an ajax request

--- components/admin/InternalReports.tsx
+++ components/admin/InternalReports.tsx
@@ -1,21 +1,21 @@
-import { Form, Formik } from 'formik';
-import router from 'next/router';
-import React from 'react';
-import { downloadInternalReport } from '../../lib/gateways/internal-api';
+import { withFormik } from 'formik';
+import React, { useRef } from 'react';
 import Button from '../button';
 import DateInput from '../form/dateinput';
 import Radios from '../form/radios';
 
-interface FormValues {
-  reportType: string;
-  startDate: string;
-  endDate: string;
-}
-
 export default function InternalReports(): JSX.Element {
+  const internalReportForm = useRef<HTMLFormElement | null>(null);
+
+  const doHtmlFormPost = () => {
+    if (internalReportForm.current) {
+      internalReportForm.current.submit();
+    }
+  };
+
   const runDate = new Date();
 
-  const initialValues: FormValues = {
+  const initialValues = {
     reportType: '0',
     startDate: new Date(
       runDate.getFullYear(),
@@ -29,38 +29,36 @@ export default function InternalReports(): JSX.Element {
     ).toDateString(),
   };
 
-  function handleSubmit(form: FormValues) {
-    const payload = {
-      ReportType: parseInt(form.reportType),
-      StartDate: new Date(form.startDate).toISOString().split('T')[0],
-      EndDate: new Date(form.endDate).toISOString().split('T')[0],
-    };
+  const InternalReportForm = withFormik({
+    handleSubmit: doHtmlFormPost,
+    mapPropsToValues: () => initialValues,
+  })(({ handleSubmit }) => {
+    return (
+      <form
+        action="/api/reports/internal/download"
+        method="POST"
+        ref={internalReportForm}
+        onSubmit={handleSubmit}
+      >
+        <Radios
+          label="Report"
+          name="ReportType"
+          options={[
+            { label: 'Case log', value: '0' },
+            { label: 'Resident log', value: '1' },
+            { label: 'Case activity log', value: '2' },
+            { label: 'Officer activity log', value: '3' },
+          ]}
+        />
 
-    downloadInternalReport(payload);
-  }
+        <DateInput name={'StartDate'} label={'Start date'} />
 
-  return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      {() => (
-        <Form>
-          <Radios
-            label="Report"
-            name="reportType"
-            options={[
-              { label: 'Case log', value: '0' },
-              { label: 'Resident log', value: '1' },
-              { label: 'Case activity log', value: '2' },
-              { label: 'Officer activity log', value: '3' },
-            ]}
-          />
+        <DateInput name={'EndDate'} label={'End date'} />
 
-          <DateInput name={'startDate'} label={'Start date'} />
+        <Button type="submit">Download CSV file</Button>
+      </form>
+    );
+  });
 
-          <DateInput name={'endDate'} label={'End date'} />
-
-          <Button type="submit">Download CSV file</Button>
-        </Form>
-      )}
-    </Formik>
-  );
+  return <InternalReportForm />;
 }

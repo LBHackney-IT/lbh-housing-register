076b495f32160d37e59f526a1850d9ea840effbb
Matt Dendle <dendle@users.noreply.github.com>
2022-08-19 15:06:41 +0100

    Revert "bidding number errors surfaced"

--- pages/api/applications/[id]/index.tsx
+++ pages/api/applications/[id]/index.tsx
@@ -7,7 +7,6 @@ import {
   hasStaffPermissions,
   isStaffAction,
 } from '../../../../lib/utils/requestAuth';
-import axios, { AxiosError } from 'axios';
 
 const endpoint: NextApiHandler = async (
   req: NextApiRequest,
@@ -30,23 +29,11 @@ const endpoint: NextApiHandler = async (
             .status(StatusCodes.FORBIDDEN)
             .json({ message: 'Unable to update application' });
         }
-      } catch (error: any | AxiosError) {
-        if (axios.isAxiosError(error)) {
-          if (error.response) {
-            // Request made and server responded
-            res.status(error.response.status).json(error.response.data);
-          } else if (error.request) {
-            // The request was made but no response was received
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-        } else {
-          res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ message: 'Unable to update application' });
-        }
+      } catch (error) {
+        console.error(error);
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ message: 'Unable to update application' });
       }
       break;
 

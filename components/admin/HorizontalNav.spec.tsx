3e18677f68e8934b4de15d9505f2bd00a2113db2
LBHTKarki <tuomo.karki@hackney.gov.uk>
2024-08-05 12:25:31 +0100

    update jest setup

---
+++ components/admin/HorizontalNav.spec.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { HorizontalNavItem } from './HorizontalNav';
+
+describe('HorizontalNavItem', () => {
+  it('displays the correct button label', () => {
+    const expectedLabel = 'New applications';
+
+    render(
+      <HorizontalNavItem itemName={'btn-name'} handleSelectNavItem={jest.fn()}>
+        {expectedLabel}
+      </HorizontalNavItem>
+    );
+
+    expect(screen.getByText(expectedLabel)).toBeInTheDocument();
+  });
+});

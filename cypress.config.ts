bdb8ff858a959ef42a8937194d8c6e236e5aa51c
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-08-06 09:19:22 +0100

    Test config

--- cypress.config.ts
+++ cypress.config.ts
@@ -20,5 +20,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    screenshotOnRunFailure: true,
   },
 });

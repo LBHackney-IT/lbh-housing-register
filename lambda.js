71a659467546e008f3a3505d7b2cb20ea1b598da
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-08-07 10:23:30 +0100

    Change precommit hook

--- lambda.js
+++ lambda.js
@@ -1,7 +1,9 @@
 const server = require('restana')();
 const app = require('next')({ dev: false });
 const files = require('serve-static');
+
 const path = require('path');
+
 const nextRequestHandler = app.getRequestHandler();
 
 server.use(files(path.join(__dirname, 'build')));

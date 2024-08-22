125e51f1a48f751eedd591ee6074a033ed140568
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-08-22 11:19:45 +0100

    Updates test to cover different googleAuth object

--- types/nested-object-diff.d.ts
+++ types/nested-object-diff.d.ts
@@ -1,12 +1,3 @@
-export interface Difference {
-  type: string;
-  path: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  lhs: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  rhs: any;
-}
-
 declare module 'nested-object-diff' {
   export function diff(a: unknown, b: unknown): Difference[];
 }

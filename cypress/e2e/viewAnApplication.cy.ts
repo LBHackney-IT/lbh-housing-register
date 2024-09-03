66ac0e9f2f2c3771d3dcb1d988eafe5ea881916e
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-09-03 12:21:01 +0100

    Testing reimplementing the screen presets

--- cypress/e2e/viewAnApplication.cy.ts
+++ cypress/e2e/viewAnApplication.cy.ts
@@ -1,51 +1,52 @@
 import ApplicationsPage from '../pages/applications';
-// import { screenPresets } from '../support/helpers';
+import { screenPresets } from '../support/helpers';
 
 describe('Application view page', () => {
-  // screenPresets.forEach((screenPreset) => {
-  //   it(`as a read only group user I cannot edit application details on ${screenPreset}`, () => {
-  //     cy.viewport(screenPreset);
+  screenPresets.forEach((screenPreset) => {
+    it(`as a read only group user I cannot edit application details on ${screenPreset}`, () => {
+      cy.task('clearNock');
+      cy.clearCookies();
+      cy.viewport(screenPreset);
+      // it(`as a read only group user I cannot edit application details on`, () => {
+      cy.loginAsUser('readOnly');
 
-  it(`as a read only group user I cannot edit application details on`, () => {
-    cy.task('clearNock');
-    cy.clearCookies();
-    cy.loginAsUser('readOnly');
+      ApplicationsPage.visit();
+      ApplicationsPage.getSearchInput().should('be.visible');
+      ApplicationsPage.getSearchInput().type('1');
+      ApplicationsPage.getSearchSubmitButton().click();
+      ApplicationsPage.getSearchResultsBox().should('be.visible');
 
-    ApplicationsPage.visit();
-    ApplicationsPage.getSearchInput().should('be.visible');
-    ApplicationsPage.getSearchInput().type('1');
-    ApplicationsPage.getSearchSubmitButton().click();
-    ApplicationsPage.getSearchResultsBox().should('be.visible');
+      ApplicationsPage.getViewApplicationLink()
+        .first()
+        .invoke('attr', 'href')
+        .then((href) => {
+          const targetId = href.split('/').pop();
+          cy.task('nock', {
+            hostname: Cypress.env('ACTIVITY_HISTORY_API'),
+            method: 'GET',
+            path: `/activityhistory?targetId=${targetId}&pageSize=100`,
+            status: 200,
+            body: {
+              results: [{}],
+              paginationDetails: { hasNext: false, nextToken: '' },
+            },
+          });
 
-    ApplicationsPage.getViewApplicationLink()
-      .first()
-      .invoke('attr', 'href')
-      .then((href) => {
-        const targetId = href.split('/').pop();
-        cy.task('nock', {
-          hostname: Cypress.env('ACTIVITY_HISTORY_API'),
-          method: 'GET',
-          path: `/activityhistory?targetId=${targetId}&pageSize=100`,
-          status: 200,
-          body: {
-            results: [{}],
-            paginationDetails: { hasNext: false, nextToken: '' },
-          },
+          ApplicationsPage.getViewApplicationLink().first().click();
         });
+      ApplicationsPage.getViewApplicationPage().should('be.visible');
+      ApplicationsPage.getEditApplicantButton().should('not.exist');
+      ApplicationsPage.getEditHouseholdMemberButton().should('not.exist');
+      ApplicationsPage.getSensitiveDataButton().should('not.exist');
+      ApplicationsPage.getChangeApplicationDateButton().should('not.exist');
+      ApplicationsPage.getChangeApplicationStatusButton().should('not.exist');
+      ApplicationsPage.getAddHouseholdMemberButton().should('not.exist');
 
-        ApplicationsPage.getViewApplicationLink().first().click();
-      });
-    ApplicationsPage.getViewApplicationPage().should('be.visible');
-    ApplicationsPage.getEditApplicantButton().should('not.exist');
-    ApplicationsPage.getEditHouseholdMemberButton().should('not.exist');
-    ApplicationsPage.getSensitiveDataButton().should('not.exist');
-    ApplicationsPage.getChangeApplicationDateButton().should('not.exist');
-    ApplicationsPage.getChangeApplicationStatusButton().should('not.exist');
-    ApplicationsPage.getAddHouseholdMemberButton().should('not.exist');
-
-    cy.task('clearNock');
+      cy.task('clearNock');
+    });
   });
 });
+
 // screenPresets.forEach((screenPreset) => {
 //   it(`as a manager group user I can edit all application details on ${screenPreset}`, () => {
 //     cy.viewport(screenPreset);
@@ -88,4 +89,4 @@ describe('Application view page', () => {
 //     cy.task('clearNock');
 //   });
 // });
-// });
+// })

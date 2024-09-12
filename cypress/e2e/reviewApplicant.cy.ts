1b5b80380dfebf7dee16f2bcb62ff22b50945da4
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-09-12 15:15:49 +0100

    run outside of the page folder

---
+++ cypress/e2e/reviewApplicant.cy.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+
+import { generateApplication } from '../../testUtils/applicationHelper';
+import ReviewApplicantPage from '../pages/reviewApplicant';
+
+const personId = faker.string.uuid();
+const applicationId = faker.string.uuid();
+
+const application = generateApplication(applicationId, personId);
+
+describe('Review applicant details', () => {
+  it('shows correct tabs for read only users', () => {
+    cy.task('clearNock');
+    cy.clearAllCookies();
+    cy.loginAsUser('readOnly');
+    cy.mockHousingRegisterApiGetApplications(applicationId, application);
+
+    ReviewApplicantPage.visit(applicationId, personId);
+    ReviewApplicantPage.getMoneySectionNavLink().should('be.visible');
+    ReviewApplicantPage.getHealthSectionNavLink().should('be.visible');
+    ReviewApplicantPage.getViewDocumentsButton().should('not.exist');
+  });
+});
---
+++ cypress/e2e/viewApplication.cy.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker';
+
+import { ApplicationStatus } from '../../lib/types/application-status';
+import { generateApplication } from '../../testUtils/applicationHelper';
+import ViewApplicationPage from '../pages/viewApplication';
+
+const applicationId = faker.string.uuid();
+const personId = faker.string.uuid();
+const application = generateApplication(applicationId, personId);
+//ensure application requires assessment
+application.status = ApplicationStatus.SUBMITTED;
+
+describe('View a resident application', () => {
+  it('does not show the assessment area for read only users', () => {
+    cy.task('clearNock');
+    cy.clearAllCookies();
+    cy.loginAsUser('readOnly');
+
+    cy.mockHousingRegisterApiGetApplications(applicationId, application);
+    cy.mockActivityHistoryApiEmptyResponse(applicationId);
+    ViewApplicationPage.visit(applicationId);
+    ViewApplicationPage.getAssessmentNavLink().should('not.exist');
+  });
+});

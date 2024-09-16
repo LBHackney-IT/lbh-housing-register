98e985c8f8a4f858f564bddcfebe4796114ee46d
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-09-16 09:50:32 +0100

    Reverts test reporters

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'results/coverage',
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['/node_modules/', '/testUtils'],

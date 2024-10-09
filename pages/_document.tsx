c7a629d6be7a2f07d70f576856fb77184f65ea14
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-10-09 09:51:46 +0100

    Adds updated gtag config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 import { ReactElement } from 'react';
 
 export default class AppDocument extends Document {
   render(): ReactElement {
     return (
       <Html id="root" lang="en">
-        <Head>
-          {process.env.NEXT_PUBLIC_ENV === 'production' ? (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","GTM-P3HRQWH");`,
-              }}
-            />
-          ) : null}
-        </Head>
+        <Head />
         <body className="govuk-template__body lbh-template__body js-enabled">
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-P3HRQWH"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            }}
+          />
         </body>
       </Html>
     );

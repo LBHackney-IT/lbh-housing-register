d117ace4207c4ee19315035eca85460cb85e1659
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-10-17 12:43:27 +0200

    Uncomment

--- cypress/e2e/pages/apply/submit/additional-questions.cy.ts
+++ cypress/e2e/pages/apply/submit/additional-questions.cy.ts
@@ -32,93 +32,93 @@ describe('Additional questions', () => {
     cy.task('clearNock');
   });
 
-  // it('shows the loading spinner while application data is being fetched on the background', () => {
-  //   cy.loginAsResident(applicationId, true);
-  //   cy.mockHousingRegisterApiGetApplications(
-  //     applicationId,
-  //     application,
-  //     false,
-  //     apiResponseDelay
-  //   );
+  it('shows the loading spinner while application data is being fetched on the background', () => {
+    cy.loginAsResident(applicationId, true);
+    cy.mockHousingRegisterApiGetApplications(
+      applicationId,
+      application,
+      false,
+      apiResponseDelay
+    );
 
-  //   AdditionalQuestionsPage.visit();
-  //   AdditionalQuestionsPage.getAdditionalQuestionsPage().should('be.visible');
+    AdditionalQuestionsPage.visit();
+    AdditionalQuestionsPage.getAdditionalQuestionsPage().should('be.visible');
 
-  //   cy.contains('Checking information…');
-  // });
+    cy.contains('Checking information…');
+  });
 
-  // it('lets user fill in and submit their personal details when logged in', () => {
-  //   cy.loginAsResident(applicationId, true);
+  it('lets user fill in and submit their personal details when logged in', () => {
+    cy.loginAsResident(applicationId, true);
 
-  //   cy.mockHousingRegisterApiGetApplications(
-  //     applicationId,
-  //     applicationWithMainApplicant
-  //   );
+    cy.mockHousingRegisterApiGetApplications(
+      applicationId,
+      applicationWithMainApplicant
+    );
 
-  //   cy.mockHousingRegisterApiPatchApplication(
-  //     applicationId,
-  //     applicationWithMainApplicant,
-  //     apiResponseDelay
-  //   );
+    cy.mockHousingRegisterApiPatchApplication(
+      applicationId,
+      applicationWithMainApplicant,
+      apiResponseDelay
+    );
 
-  //   cy.mockHousingRegisterApiGetApplications(
-  //     applicationId,
-  //     applicationWithMainApplicant
-  //   );
+    cy.mockHousingRegisterApiGetApplications(
+      applicationId,
+      applicationWithMainApplicant
+    );
 
-  //   AdditionalQuestionsPage.visit();
-  //   AdditionalQuestionsPage.getAdditionalQuestionsPage().should('be.visible');
+    AdditionalQuestionsPage.visit();
+    AdditionalQuestionsPage.getAdditionalQuestionsPage().should('be.visible');
 
-  //   Components.getConditionalCheckboxes().first().click();
-  //   AdditionalQuestionsPage.getAdditionalQuestionsNotes()
-  //     .first()
-  //     .scrollIntoView()
-  //     .click()
-  //     .type(faker.lorem.sentence());
+    Components.getConditionalCheckboxes().first().click();
+    AdditionalQuestionsPage.getAdditionalQuestionsNotes()
+      .first()
+      .scrollIntoView()
+      .click()
+      .type(faker.lorem.sentence());
 
-  //   Components.getSaveButton().click();
+    Components.getSaveButton().click();
 
-  //   cy.contains('Saving...');
+    cy.contains('Saving...');
 
-  //   Components.getLoadingSpinner().should('not.exist');
-  //   EthnicityPage.getEthnicityPage().should('be.visible');
-  // });
+    Components.getLoadingSpinner().should('not.exist');
+    EthnicityPage.getEthnicityPage().should('be.visible');
+  });
 
-  // it('shows an error message when action fails', () => {
-  //   cy.loginAsResident(applicationId, true);
+  it('shows an error message when action fails', () => {
+    cy.loginAsResident(applicationId, true);
 
-  //   cy.mockHousingRegisterApiGetApplications(
-  //     applicationId,
-  //     applicationWithMainApplicant
-  //   );
+    cy.mockHousingRegisterApiGetApplications(
+      applicationId,
+      applicationWithMainApplicant
+    );
 
-  //   const errorStatusCode = StatusCodes.BAD_REQUEST;
+    const errorStatusCode = StatusCodes.BAD_REQUEST;
 
-  //   const expectedErrorMessage = `Unable to update application (${errorStatusCode})`;
+    const expectedErrorMessage = `Unable to update application (${errorStatusCode})`;
 
-  //   AdditionalQuestionsPage.visit();
+    AdditionalQuestionsPage.visit();
 
-  //   AdditionalQuestionsPage.getErrorSummary().should('not.exist');
+    AdditionalQuestionsPage.getErrorSummary().should('not.exist');
 
-  //   Components.getConditionalCheckboxes().first().click();
-  //   AdditionalQuestionsPage.getAdditionalQuestionsNotes()
-  //     .first()
-  //     .scrollIntoView()
-  //     .click()
-  //     .type(faker.lorem.sentence());
+    Components.getConditionalCheckboxes().first().click();
+    AdditionalQuestionsPage.getAdditionalQuestionsNotes()
+      .first()
+      .scrollIntoView()
+      .click()
+      .type(faker.lorem.sentence());
 
-  //   cy.mockHousingRegisterApiPatchApplication(
-  //     applicationId,
-  //     applicationWithMainApplicant,
-  //     apiResponseDelay,
-  //     errorStatusCode
-  //   );
+    cy.mockHousingRegisterApiPatchApplication(
+      applicationId,
+      applicationWithMainApplicant,
+      apiResponseDelay,
+      errorStatusCode
+    );
 
-  //   Components.getSaveButton().click();
+    Components.getSaveButton().click();
 
-  //   AdditionalQuestionsPage.getErrorSummary().should('be.visible');
-  //   cy.contains(expectedErrorMessage);
-  // });
+    AdditionalQuestionsPage.getErrorSummary().should('be.visible');
+    cy.contains(expectedErrorMessage);
+  });
   it('shows an error message when dispatch fails', () => {
     cy.loginAsResident(applicationId, true);
 

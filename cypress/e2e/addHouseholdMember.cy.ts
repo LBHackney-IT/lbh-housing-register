23a726407f888c25c50d9ba8780bef4e8e0b78c4
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-10-29 19:09:34 +0000

    Clean tests

--- cypress/e2e/addHouseholdMember.cy.ts
+++ cypress/e2e/addHouseholdMember.cy.ts
@@ -8,6 +8,5 @@ describe('Add household member', () => {
     cy.loginAsUser('readOnly');
     cy.visit(`applications/edit/${applicationId}/add-household-member`);
     cy.contains('Access denied');
-    cy.contains('fkjsdklfjasd');
   });
 });

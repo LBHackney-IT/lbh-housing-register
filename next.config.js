39d25de506cca2d9baddbb5b7aa0689270877505
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-11-04 09:46:54 +0000

    Adjust sentry config

--- next.config.js
+++ next.config.js
@@ -49,21 +49,10 @@ const nextConfig = {
   },
 };
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.NEXT_PUBLIC_ENV === 'localdev',
-//   openAnalyzer: false,
-// });
-
+// Note the sentry/nextjs uses webpack under the hood.
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
   silent: true, // Suppresses all logs
-  dryRun: process.env.NEXT_PUBLIC_ENV !== 'production',
-  hideSourceMaps: true, // Sentry CLI will not upload the source maps
+  hideSourceMaps: true, // Will make sourcemaps invisible to the browser.
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
@@ -71,6 +60,3 @@ const sentryWebpackPluginOptions = {
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-// withBundleAnalyzer(nextConfig),
-// sentryWebpackPluginOptions
-// );

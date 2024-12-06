ed0189c5032fbee907650e991628e0fd2e6e67d7
Sam Drumm <sam.drumm@hackney.gov.uk>
2024-12-06 22:27:22 +1300

    Updates the analytics config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 import { ReactElement } from 'react';
 
@@ -6,15 +7,26 @@ export default class AppDocument extends Document {
   render(): ReactElement {
     return (
       <Html id="root" lang="en">
-        <Head />
-        <body className="govuk-template__body lbh-template__body js-enabled">
-          <Main />
-          <NextScript />
-          <noscript
+        <Head>
+          <Script
+            id="gtm"
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);})(window,document,"script","dataLayer","${process.env.NEXT_PUBLIC_GTM_ID}");`,
             }}
           />
+        </Head>
+        <body className="govuk-template__body lbh-template__body js-enabled">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+              title="Google Tag Manager"
+            ></iframe>
+          </noscript>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );

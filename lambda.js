2487b49337e3c791ac11810bdcac969cffcc1f3a
Sam Drumm <sam.drumm@hackney.gov.uk>
2026-04-10 09:06:52 +0100

    chore: test on branch

--- lambda.js
+++ lambda.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const { createRequire } = require('module');
 
 // Next standalone output (see next.config.js output: 'standalone', distDir: 'build/_next')
 const standaloneDir = path.join(__dirname, 'build', '_next', 'standalone');
+// `process.chdir` does NOT change `require()` resolution; deps live under standalone/node_modules.
+const requireStandalone = createRequire(
+  path.join(standaloneDir, 'package.json'),
+);
+
 process.chdir(standaloneDir);
 
-const server = require('restana')();
-const next = require('next');
-const files = require('serve-static');
+const server = requireStandalone('restana')();
+const next = requireStandalone('next');
+const files = requireStandalone('serve-static');
 
 const app = next({ dev: false, dir: standaloneDir });
 const nextRequestHandler = app.getRequestHandler();
@@ -15,7 +21,7 @@ server.use(files(path.join(standaloneDir, 'public')));
 
 server.all('*', (req, res) => nextRequestHandler(req, res));
 
-const serverless = require('serverless-http');
+const serverless = requireStandalone('serverless-http');
 
 let prepared = false;
 let handler;

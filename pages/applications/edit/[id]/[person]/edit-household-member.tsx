ce61ff3910b57e3d7e499995b48b982b2574e156
Sam Drumm <sam.drumm@hackney.gov.uk>
2026-07-31 11:37:28 +0100

    ci: remove incorrectly labeled next_public vars

--- pages/applications/edit/[id]/[person]/edit-household-member.tsx
+++ pages/applications/edit/[id]/[person]/edit-household-member.tsx
@@ -160,6 +160,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
-  const evidenceLink = process.env.NEXT_PUBLIC_EVIDENCE_STORE || null;
+  const evidenceLink = process.env.EVIDENCE_STORE_URL || null;
   return { props: { user, data, person, evidenceLink } };
 };
